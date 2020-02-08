8aaab38e509c1c32de1407af53e17939343a7e57
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-02-09 01:10:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,10 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/small/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/R20.1.24/litte/g' package/lean/default-settings/files/zzz-default-settings
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-git clone https://github.com/kenzok8/openwrt-packages.git package/lean/openwrt-packages
+sed -i 's/"DISTRIB_DESCRIPTION='OpenWrt '"/"DISTRIB_DESCRIPTION='small '"/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/"DISTRIB_REVISION='R20.1.24'"/"DISTRIB_REVISION='litte'"/g' package/lean/default-settings/files/zzz-default-settings
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 ./scripts/feeds clean
 ./scripts/feeds update -a
 rm -rf feeds/lienol/package/v2ray
