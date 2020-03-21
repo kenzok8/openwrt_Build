ffb4ed7642390270ba0b3b617d65a8165a77ca13
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-21 16:20:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -14,8 +14,9 @@ rm -rf package/litte/lluci-app-ssr-plus
 rm -rf package/litte/microsocks
 rm -rf package/litte/redsocks2
 rm -rf package/openwrt-package/lienol/luci-theme-bootstrap-mod
-sed -i 's/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Lean '/g' package/base-files/files/etc/openwrt_release
-sed -i 's/DISTRIB_REVISION='R20.3.11'/DISTRIB_REVISION='small'/g' package/base-files/files/etc/openwrt_release
+sed -i 's/LuCI Master/LuCI master by kenzo/g' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 ./scripts/feeds update -a
 ./scripts/feeds install
