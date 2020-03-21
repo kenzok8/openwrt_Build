3c90f02c9825e53b16919386012781542d6ac204
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-21 23:19:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
+# Description: Build OpenWrt using GitHub Actions
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: kenzo
+# https://github.com/kenzok8
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
