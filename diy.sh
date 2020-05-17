72c008522a895e000b923b5873a4b58c0a2ae5aa
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-17 22:26:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,10 +8,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
+git clone https://github.com/kenzok8/small.git
 rm -rf package/lean/luci-theme-argon
-sed -i 's/LuCI Master/LuCI master by kenzo/g' /usr/lib/lua/luci/version.lua
-sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
+sed -i 's/LuCI Master/by kenzo/g' /usr/lib/lua/luci/version.lua
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 ./scripts/feeds update -a
 ./scripts/feeds install
