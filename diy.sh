ce3bb2c5a7cdc9a188c4181491ed39241bade7e0
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-23 17:31:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ cd lede/package
 git clone https://github.com/kenzok8/litte.git
 git clone https://github.com/kenzok8/small.git
 date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by GaryPang'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mkootstrap
 ./scripts/feeds update -a
 ./scripts/feeds install -a
