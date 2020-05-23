f49f9ced61a02db8ea8e8a96956039cf4482e08b
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-23 17:29:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,9 @@ sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generat
 cd lede/package
 git clone https://github.com/kenzok8/litte.git
 git clone https://github.com/kenzok8/small.git
-rm -rf package/lean/luci-theme-argon
-sed -i 's/LuCI Master/by kenzo/g' /usr/lib/lua/luci/version.lua
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+date=`date +%m.%d.%Y`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by GaryPang'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mkootstrap
 ./scripts/feeds update -a
-./scripts/feeds install
+./scripts/feeds install -a
+cd lede
