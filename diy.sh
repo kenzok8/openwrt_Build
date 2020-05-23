dbfd63bbed5299cbb311c3dedf940b41646f8ddf
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-23 19:45:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,12 +7,12 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-cd lede/package
-git clone https://github.com/kenzok8/litte.git
-git clone https://github.com/kenzok8/small.git
+git clone https://github.com/kenzok8/litte.git package/litte
+rm -rf package/litte/luci-app-passwall && rm -rf package/lean/luci-app-ssr-plus
+rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
 date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mkootstrap
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
-./scripts/feeds install -a
-cd lede
+./scripts/feeds install
