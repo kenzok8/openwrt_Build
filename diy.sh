37f9f2c1308fd1f47b2842275caa5cea59793376
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-24 16:44:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
-git clone https://github.com/kenzok8/small.git package/lsmall
+git clone https://github.com/kenzok8/small.git package/small
 rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
