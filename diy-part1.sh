190254fb5f7c2d92074d71ece3882dde91dd5fab
kenzok8 <kenzok8@gmail.com>
2020-05-25 22:05:44 +0800

    Signed-off-by: kenzo

--- diy-part1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#=============================================================
-#取消注释提供源
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-添加自定义源
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
