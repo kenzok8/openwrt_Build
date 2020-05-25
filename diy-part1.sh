116929ab2beb0bc121a995eba403c72465f6b138
kenzok8 <kenzok8@gmail.com>
2020-05-25 20:54:06 +0800

    Signed-off-by: kenzo

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=============================================================
-
 #取消注释提供源
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
