292cc508f89de47c9694676bef19112924c79def
kenzok8 <kenzok8@gmail.com>
2020-05-26 20:26:37 +0800

    Signed-off-by: kenzo

--- customize.sh
+++ customize.sh
@@ -4,9 +4,10 @@
 # Author: kenzo
 # https://github.com/kenzok8
 #=================================================
-cp -f package/litte/default-settings package/*/*/default-settings/files/zzz-default-settings
+cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
 cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 ./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+
 
 
