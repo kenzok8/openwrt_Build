f05ba8b4a7fea137f22f8a76da881e8eb1b8d9c0
kenzok8 <kenzok8@gmail.com>
2020-05-26 20:07:00 +0800

    Signed-off-by: kenzo

--- Lean.sh
+++ Lean.sh
@@ -4,17 +4,9 @@
 # Author: kenzo
 # https://github.com/kenzok8
 #=================================================
-#取消注释提供源
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#添加自定义源Lean运行下列命令
-#sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-rm -rf package/lean/luci-app-ssr-plus && rm -rf package/litte/luci-app-ssr-plus
-rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
-date=`date +%m.%d.%Y`
 cp -f package/litte/default-settings package/*/*/default-settings/files/zzz-default-settings
-cp -f package/litte/banner package/*/*/base-files/files/etc/banner
-cp -f package/litte/Leandiffconfig diffconfig
-./scripts/feeds update -a
-./scripts/feeds install -a
+cp -f package/litte/banner package/base-files/files/etc/banner
+#cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
+./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+
 
--- ctc.sh
+++ ctc.sh
@@ -4,10 +4,10 @@
 # Author: kenzo
 # https://github.com/kenzok8
 #=================================================
-取消注释提供源
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-添加自定义源
-sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
+#取消注释提供源
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#添加自定义源
+#sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
 rm -rf package/lienol/luci-app-passwall && rm -rf package/lean/luci-app-ssr-plus && rm -rf package/litte/luci-app-ssr-plus
 rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
 rm -rf package/litte/adguardhome && rm -rf package/litte/luci-app-adguardhome
@@ -15,9 +15,9 @@ rm -rf package/litte/luci-app-advancedsetting && rm -rf package/litte/luci-app-a
 rm -rf package/litte/gost && rm -rf package/litte/luci-app-gost && rm -rf package/litte/luci-app-eqos
 rm -rf package/ctcgfw/luci-app-openclash && rm -rf package/litte/luci-app-smartdns && rm -rf package/litte/smartdns
 rm -rf package/ctcgfw/luci-theme-atmaterial && rm -rf package/ctcgfw/luci-theme-opentomato && rm -rf package/ctcgfw/luci-theme-opentomcat
-date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
-cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
-./scripts/feeds update -a
-./scripts/feeds install -a
+cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
+cp -f package/litte/banner package/base-files/files/etc/banner
+#cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
+./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+
+
