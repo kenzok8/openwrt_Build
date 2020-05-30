e4bfb28862da17ea61fca68dba0bea3a086024bc
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-31 01:11:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,8 +8,8 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 自定义设置
-cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
-cp -f package/litte/banner package/base-files/files/etc/banner
+#cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
+#cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
-./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+#./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
 
