4cf2a8e78af952e60c974a7118530a56b0de42af
kenzok8 <kenzok8@gmail.com>
2021-04-17 20:14:09 +0800

    测试customize.sh的编辑功能

--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@
 # Lisence: MIT
 # Author: kenzo
 #=================================================
-#1. Modify default IP
+#1. 自定义 IP
 sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 自定义设置
@@ -12,4 +12,11 @@ sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config
 #cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 #./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+
+
+#3. 固件diy设置
+sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
+date=`date +%m.%d.%Y`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 
