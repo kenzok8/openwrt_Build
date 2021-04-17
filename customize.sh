dc56af437df56a17043cc923664f5320fdcd4582
有種 <39034242+kenzok8@users.noreply.github.com>
2021-04-18 01:53:26 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,19 +2,20 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: kenzo
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. 自定义 IP
+#1. Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. 自定义设置
+#2. Custom settings
 #cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
 #cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 #./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
 
 
-#3. 固件diy设置
+#3. Firmware diy settings
 sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
