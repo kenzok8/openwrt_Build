392508062619b5c39fb0c7dbf857829a41ad01b1
有種 <39034242+kenzok8@users.noreply.github.com>
2021-04-18 13:06:16 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,10 +14,3 @@ sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 #./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
 
-
-#3. Firmware diy settings
-sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
-date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
-
