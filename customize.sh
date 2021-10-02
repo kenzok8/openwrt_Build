d479e76d0d8e419fec61d68e411ed3b7b6183740
有種 <39034242+kenzok8@users.noreply.github.com>
2021-10-02 14:47:44 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,10 +3,14 @@
 shopt -s extglob
 sed -i '/	refresh_config();/d' scripts/feeds
 
-sma=$(date "+%Y-%m-%d")
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt by kenzo'\"/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"$sma'\"/g" package/base-files/files/etc/openwrt_release
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
+#2. Custom settings
+
+date=`date +%m.%d`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt by kenzo'\"/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"$date'\"/g" package/base-files/files/etc/openwrt_release
 #Lean设置
 sed -i "s/LuCI Master.*/LuCI Master=\"kenzo'\"/g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/luciversion.*/luciversion=\"Sept'\"/g" package/lean/default-settings/files/zzz-default-settings
@@ -17,3 +21,5 @@ sed -i "s/luciversion.*luciversion=\"Sept'\"/g" package/emortal/default-settings
 #Lienol设置
 sed -i "s/17.01 Lienol.*/17.01 Lienol=\"kenzo'\"/g" package/default-settings/files/zzz-default-settings
 sed -i "s/luciversion.*/luciversion=\"Sept'\"/g" package/default-settings/files/zzz-default-settings
+
+exit
