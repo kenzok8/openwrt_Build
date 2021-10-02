151ce8c6d130fcfcb5d8278b754a7ea9c9c425c3
有種 <39034242+kenzok8@users.noreply.github.com>
2021-10-02 14:39:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+shopt -s extglob
+sed -i '/	refresh_config();/d' scripts/feeds
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Custom settings
-#cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
-#cp -f package/litte/banner package/base-files/files/etc/banner
-#cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
-#./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+#2. Custom setting
+sma=$(date "+%Y-%m-%d")
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt by kenzo'\"/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"sma'\"/g" package/base-files/files/etc/openwrt_release
 
+#Lean设置
+sed -i "s/LuCI Master.*/LuCI Master=\"kenzo'\"/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*/luciversion=\"Sept'\"/g" package/lean/default-settings/files/zzz-default-settings
+#ctc设置
+sed -i "s/LuCI openwrt-18.06 branch.*LuCI openwrt-18.06 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
+sed -i "s/LuCI openwrt-21.02 branch.*LuCI openwrt-21.02 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*luciversion=\"Sept'\"/g" package/emortal/default-settings/files/zzz-default-settings
+#Lienol设置
+sed -i "s/17.01 Lienol.*/17.01 Lienol=\"kenzo'\"/g" package/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*/luciversion=\"Sept'\"/g" package/default-settings/files/zzz-default-settings
+
+exit
