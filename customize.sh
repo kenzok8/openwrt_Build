25614529f0b031fc04e5c1c3aa5481c6642b6f0c
有種 <39034242+kenzok8@users.noreply.github.com>
2022-02-28 16:06:06 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,13 +6,6 @@ sed -i '/	refresh_config();/d' scripts/feeds
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
 
-echo '
-CONFIG_CRYPTO_CHACHA20_X86_64=y
-CONFIG_CRYPTO_POLY1305_X86_64=y
-CONFIG_DRM=y
-CONFIG_DRM_I915=y
-' >> ./target/linux/x86/config-5.10
-
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
