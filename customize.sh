cea38b0cce24f865ae0726d4dde6b17ecb52b8c9
有種 <39034242+kenzok8@users.noreply.github.com>
2022-03-20 22:11:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#=================================================
-shopt -s extglob
-sed -i '/	refresh_config();/d' scripts/feeds
-
+#===============================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
 
